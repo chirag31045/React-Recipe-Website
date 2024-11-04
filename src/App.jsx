171783a4843c71b56594d7913a656fbb3f1fc4f8
chiragkumawat <chiragkumawat457@gmail.com>
2024-11-04 21:52:51 +0530

    React Recipe Website

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Meal from './Food_Recipe/Meal'
+
+const App = () => {
+  return (
+    <>
+    <div>
+     <Meal/>
+
+   </div>
+    </>
+    
+  )
+}
+
+export default App
